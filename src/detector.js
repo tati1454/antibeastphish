0410b1fa54dc66d9eff4f988c768328c75d03055
Tati <45135760+tati1454@users.noreply.github.com>
2021-07-16 07:39:28 -0300

    Initial commit

---
+++ src/detector.js
@@ -0,0 +1,31 @@
+const dns = require("dns").promises
+
+const beastphishIPs = [
+    "194.226.139.111",
+    "194.226.139.112",
+    "194.226.139.121",
+    "194.226.139.129",
+    "194.226.139.130"
+]
+
+function getUrlsInString(input){
+    let re = /https?:\/\/(?:[-\w.]|(?:%[\da-fA-F]{2}))+/
+    let output = re.exec(input)
+
+    return output;
+}
+
+async function resolveUrlIp(input){
+    const inputUrl = new URL("/", input)
+
+    let address = await dns.resolve4(inputUrl.hostname)
+
+    return address[0]
+}
+
+async function isBeatPhishUrl(urlString){
+    const ip = await resolveUrlIp(urlString)
+
+    return beastphishIPs.includes(ip)
+}
+
